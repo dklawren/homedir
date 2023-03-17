8d81b6c653a797f83b9292fc08d3e607a4f55e39
David Lawrence <dkl@mozilla.com>
2023-03-17 16:18:58 -0400

    Lunarvim changes

--- dotfiles/.config/lvim/config.lua
+++ dotfiles/.config/lvim/config.lua
@@ -74,6 +74,7 @@ lvim.builtin.treesitter.ensure_installed = {
   "c",
   "css",
   "html",
+  "http",
   "javascript",
   "json",
   "lua",
@@ -187,6 +188,57 @@ lvim.plugins = {
       config = function()
         require("auto-save").setup()
       end,
+    },
+    {
+      'pwntester/octo.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+        'kyazdani42/nvim-web-devicons',
+      },
+      config = function ()
+        require"octo".setup()
+      end
+    },
+    {
+      "rest-nvim/rest.nvim",
+      requires = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("rest-nvim").setup({
+          -- Open request results in a horizontal split
+          result_split_horizontal = false,
+          -- Keep the http file buffer above|left when split horizontal|vertical
+          result_split_in_place = false,
+          -- Skip SSL verification, useful for unknown certificates
+          skip_ssl_verification = false,
+          -- Encode URL before making request
+          encode_url = true,
+          -- Highlight request on run
+          highlight = {
+            enabled = true,
+            timeout = 150,
+          },
+          result = {
+            -- toggle showing URL, HTTP info, headers at top the of result window
+            show_url = true,
+            show_http_info = true,
+            show_headers = true,
+            -- executables or functions for formatting response body [optional]
+            -- set them to false if you want to disable them
+            formatters = {
+              json = "jq",
+              html = function(body)
+                return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
+              end
+            },
+          },
+          -- Jump to request line on run
+          jump_to_request = false,
+          env_file = '.env',
+          custom_dynamic_variables = {},
+          yank_dry_run = true,
+        })
+      end
     }
 }
 
